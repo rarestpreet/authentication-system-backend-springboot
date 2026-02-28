98bc42d012a22f6de1c4c29de2759be234679887
arpitgoswami03 <arpit.personal.2003@gmail.com>
2026-02-28 15:37:03 +0530

    added user model and builder

---
+++ src/main/java/com/project/authentication_system/entity/CustomUserDetails.java
@@ -0,0 +1,32 @@
+package com.project.authentication_system.entity;
+
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public @NullMarked class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
--- src/main/java/com/project/authentication_system/mapper/UserMapper.java
+++ src/main/java/com/project/authentication_system/mapper/UserMapper.java
@@ -1,34 +1,37 @@
 package com.project.authentication_system.mapper;
 
-import com.project.authentication_system.dto.request.UserRequestDTO;
+import com.project.authentication_system.dto.request.RegisterRequestDTO;
 import com.project.authentication_system.dto.response.UserResponseDTO;
 import com.project.authentication_system.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
 public class UserMapper {
+
+    private final PasswordEncoder passwordEncoder;
 
     public static UserResponseDTO toDTO(User user) {
         return UserResponseDTO.builder()
                 .userId(user.getUserId())
                 .username(user.getUsername())
-                .password(user.getPassword())
                 .isAccountVerified(user.getIsAccountVerified())
                 .build();
     }
 
-    public static User toEntity(UserRequestDTO userRequestDTO) {
+    public static User toEntity(RegisterRequestDTO registerRequestDTO) {
         return User.builder()
                 .userId(UUID.randomUUID().toString())
-                .username(userRequestDTO.getUsername())
-                .password(userRequestDTO.getPassword())
-                .email(userRequestDTO.getEmail())
+                .username(registerRequestDTO.getUsername())
+                .password(registerRequestDTO.getPassword())
+                .email(registerRequestDTO.getEmail())
                 .verifyOtp(null)
                 .isAccountVerified(false)
                 .verifyOtpExpireAt(0L)
                 .resetOtp(null)
                 .resetOtpExpireAt(0L)
                 .build();
-
     }
 }
